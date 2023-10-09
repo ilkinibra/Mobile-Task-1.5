cf6eafe32256c9c4ab170fbe98fcf841025e0313
Ilkin Ibrahimov <99792912+ilkinibra@users.noreply.github.com>
2023-10-09 12:50:36 +0000

    Update note.js

--- Task 1.5/note.js
+++ Task 1.5/note.js
@@ -1,18 +1,18 @@
 const gun = 8; 
 
-if (gun == 1) {
+if (gun === 1) {
     console.log("Bazar ertesi");
-} else if (gun == 2) {
+} else if (gun === 2) {
     console.log("cerwenbe axwami");
-} else if (gun == 3) {
+} else if (gun === 3) {
     console.log("cerwenbe");
-} else if (gun == 4) {
+} else if (gun === 4) {
     console.log("cume axwami");
-} else if (gun == 5) {
+} else if (gun === 5) {
     console.log("cume");
-} else if (gun == 6) {
+} else if (gun === 6) {
     console.log("wenbe");
-} else if (gun == 7) {
+} else if (gun === 7) {
     console.log("bazar");
 } else {
     console.log(" heftede yeddi gun var!!!");
